3675f7b14c37a9369988f5e62846cfeaa56c7f2e
00757033 <58459235+00757033@users.noreply.github.com>
2019-12-14 22:46:49 +0800

    12/14 10:46

--- first.js
+++ first.js
@@ -3,30 +3,26 @@ function clickpic(e) {
     var y = event.offsetY;
     console.log(x, y);
     if (e.target.getAttribute("id") == "firstfloor") {
-        if ((x > 450 && x < 510) && (y > 158 && y < 290)) {
+        if ((x > 450 && x < 570) && (y > 210 && y < 375)) {
             e.target.setAttribute("id", "coin");
             e.target.setAttribute("class", "coin");
             ////window.location.href='machine.html'
         }
-        else if ((x > 115 && x < 160) && (y > 10 && y < 250)) {
+        else if ((x > 3 && x <50) && (y > 20 && y < 325)) {
             e.target.setAttribute("class", "list");
             e.target.setAttribute("id", "list");
             ////window.location.href='machine.html'
         }
-        else if ((x > 210 && x < 260) && (y > 208 && y < 260)) {
+        else if ((x > 140 && x < 195) && (y > 300 && y < 365)) {
             e.target.setAttribute("class", "trash_can");
             e.target.setAttribute("id", "trash_can");
             ////window.location.href='machine.html'
         }
-        else if ((x > 320 && x < 410) && (y > 305 && y < 338)) {
+        else if ((x > 280 && x < 420) && (y > 435 && y < 490)) {
             e.target.setAttribute("class", "information");
             e.target.setAttribute("id", "information");
         }
-        else if ((x > 546 && x < 575) && (y > 170 && y < 200)) {
-            e.target.setAttribute("class", "lightcontrol");
-            e.target.setAttribute("id", "lightcontrol");
-        }
-        else if ((x > 600 && x < 680) && (y > 134 && y < 312)) {
+        else if ((x > 680 && x < 750) && (y > 170 && y < 455)) {
             e.target.setAttribute("class", "stair1");
             e.target.setAttribute("id", "stair1");
         }
