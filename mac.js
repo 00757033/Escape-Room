05162613a1f077449f445f1c75eabcd920875b81
hahapiggy <58760348+hahapiggy@users.noreply.github.com>
2019-12-26 12:19:08 +0800

    二樓修改 && mac謎題

--- mac.js
+++ mac.js
@@ -1,7 +1,6 @@
 function clickpic(e) {
     var x = e.offsetX;
     var y = e.offsetY;
-    console.log(x,y,e.target.getAttribute("id"));
     if(e.target.getAttribute("id") == "class205"){   
         if ((x > 290 && x < 405) && (y > 250 && y < 355))  {
             $("#topdiv").text("點擊電腦螢幕");
@@ -18,39 +17,54 @@ function clickpic(e) {
     }
     else if(e.target.getAttribute("id") == "screen"){    
         if ((x > 565 && x <625) && (y > 185 && y < 260))  {
-            $("#topdiv").text("點擊錯誤的程式碼");
+            $("#topdiv").text("debug囉~");
             e.target.setAttribute("id", "code_f");
             e.target.setAttribute("class", "code_f");
         }
     }
-    else if(e.target.getAttribute("id") == "code_f"){           
-        if ((x > 55 && x <= 230) && (y > 220 && y < 240))  {
-            set_text(e);
-            check_ans();
+    else if(e.target.getAttribute("id") == "code_f"){     
+        if ((x > 525 && x < 685) && (y > 400 && y < 685)) {
+            e.target.setAttribute("id", "result_f");
+            e.target.setAttribute("class", "result_f");
         }
+        else if ((x >= 65 && x <= 275) && (y >= 265 && y <= 280))  {
+            set_text(e);
+        }
+    }
+    else if(e.target.getAttribute("id") == "code_t"){     
+        if ((x > 525 && x < 685) && (y > 400 && y < 685)) {
+            e.target.setAttribute("id", "result_t");
+            e.target.setAttribute("class", "result_t");
+            $("#topdiv").text("再點擊即可回到二樓大廳");
+        }
+    }
+    else if(e.target.getAttribute("id") == "result_f"){ 
+        e.target.setAttribute("id", "code_f");
+        e.target.setAttribute("class", "code_f");
+    }
+    else if(e.target.getAttribute("id") == "result_t"){ 
+        window.location.href='second.html'
     }
 }
 
 function set_text(e){
-    console.log("in set_text");
     var inner = "<input id = 'ans' type = 'text' placeholder = '請輸入正確的code' class = 'text_answer'>";
     inner += "<input id = 'ans_b' type = 'button' value = '確認輸入' class = 'button_text'>";
-    console.log(inner);
     document.getElementById("code_f").innerHTML = inner;
     document.getElementById("ans_b").addEventListener("click", check_ans, false);
 }
 
 function check_ans(){
     var result = document.getElementById("ans").value;
-    console.log("result is "+result);
-    var inner = "<div class = 'text_answer'><input id = 'ans' type = 'text' placeholder = '請輸入正確的code'></div>";
-    inner += "<div class = 'button_text'><input id = 'ans_b' type = 'button' value = '確認輸入'></div>";
     if((result === "arr[j]=arr[i];") || (result === "arr[j] = arr[i];")){
-        //inner += ""
-        window.alert("輸入正確");
+        $("#topdiv").text("恭喜你成功了~ 試著編譯看看吧");
+        var tmp = document.getElementById("code_f");
+        tmp.setAttribute("id", "code_t");
+        tmp.setAttribute("class", "code_t");        
+        document.getElementById("code_t").innerHTML = "";
+        localStorage.setItem("mac_code", 1);
     }
-    else{
-        //inner += "<div class = 'result_list_f'>更改錯誤</div>"
+    else{        
+        $("#topdiv").text("加油！ 再試試看吧");
     }
-    //document.getElementById("onmachine").innerHTML = inner;
 }
